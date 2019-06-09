a0cf6d21130ab4aa0726dc420f338e508593efad
Benedict Becker <benedict.becker@rwth-aachen.de>
2019-06-09 13:35:03 +0200

    Added main app container

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+
+const Page = styled.div`
+ display: block;
+ width: 100%;
+`;
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Page>
+        Test
+      </Page>
   );
-}
+};
 
 export default App;
