95d320516519b19ee6415c00c8f4ef53f0e97044
Benedict Becker <benedict.becker@rwth-aachen.de>
2019-06-10 18:37:08 +0200

    Fixed html font override for 1rem

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,32 @@ import Header from "./components/Header";
 
 import './utils/translation';
 import LandingPage from "./components/LandingPage";
+import Projects from "./components/Projects";
+import {createMuiTheme} from "@material-ui/core";
+import {ThemeProvider} from "@material-ui/styles";
 
 const Page = styled.div`
  display: block;
  width: 100%;
 `;
 
+const theme = createMuiTheme({
+    typography: {
+        htmlFontSize: 10
+    },
+});
+
 const App: React.FC = () => {
   return (
-      <Suspense fallback={null}>
-          <Page>
-            <Header />
-            <LandingPage />
-          </Page>
-      </Suspense>
+      <ThemeProvider theme={theme}>
+          <Suspense fallback={null}>
+              <Page>
+                <Header />
+                <LandingPage />
+                <Projects />
+              </Page>
+          </Suspense>
+      </ThemeProvider>
   );
 };
 
