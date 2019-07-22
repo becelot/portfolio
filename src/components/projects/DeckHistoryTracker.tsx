d0e19d3ccd7e3aaec79f5beac05d1609d49ff916
Benedict Becker <benedict.becker@rwth-aachen.de>
2019-07-22 21:32:57 +0200

    Added Redux technology

--- src/components/projects/DeckHistoryTracker.tsx
+++ src/components/projects/DeckHistoryTracker.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import {WithTranslation, withTranslation} from "react-i18next";
 import ProjectLayout from "./ProjectLayout";
 import ProjectDefaultContent from "./ProjectDefaultContent";
+import {Typography} from "@material-ui/core";
 
 const DeckHistoryTracker: React.FC<WithTranslation> = ({t}) => {
     return (
         <ProjectLayout
-            technologyBadges={['React', 'HTML', 'CSS', 'Python', 'Flask', 'PostgreSQL', 'C#', 'WPF']}
+            technologyBadges={['React', 'Redux', 'HTML', 'CSS', 'Python', 'Flask', 'PostgreSQL', 'JWT', 'Authorization', 'C#', 'WPF']}
             image={'projects/deck-history-tracker.jpg'}
             projectPrefix={'dht'}
         >
