6d6e42e4bae315642dbbd37bcbd718484589474d
Benedict Becker <benedict.becker@rwth-aachen.de>
2019-07-22 21:32:11 +0200

    Description of DSLViz

--- src/components/projects/DSLViz.tsx
+++ src/components/projects/DSLViz.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import {WithTranslation, withTranslation} from "react-i18next";
 import ProjectLayout from "./ProjectLayout";
 import ProjectDefaultContent from "./ProjectDefaultContent";
+import {Typography} from "@material-ui/core";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faDotCircle} from "@fortawesome/free-regular-svg-icons";
 
 const DSLViz: React.FC<WithTranslation> = ({t}) => {
     return (
@@ -10,6 +13,46 @@ const DSLViz: React.FC<WithTranslation> = ({t}) => {
             image={'projects/editor-preview.png'}
             projectPrefix={'dslviz'}
         >
+            <Typography variant={'h6'}>
+                {t('projects.dslviz.project')}
+            </Typography>
+            <Typography>
+                {t('projects.dslviz.short_description')}
+            </Typography> <br />
+            <Typography variant={'h6'}>
+                {t('projects.dslviz.problem')}
+            </Typography>
+            <Typography>
+                {t('projects.dslviz.problem_description')}
+            </Typography> <br />
+            <Typography variant={'h6'}>
+                {t('projects.dslviz.solution')}
+            </Typography>
+            <Typography>
+                {t('projects.dslviz.solution_description')}
+            </Typography> <br />
+            <div style={{paddingLeft: '24px'}}>
+            <Typography>
+                <FontAwesomeIcon icon={faDotCircle} color={'#4497e2'} size={'lg'} style={{marginRight: '1.5rem'}} />
+                {t('projects.dslviz.solution-0')}
+            </Typography>
+                <Typography style={{marginTop: '5px'}}>
+                    <FontAwesomeIcon icon={faDotCircle} color={'#4497e2'} size={'lg'} style={{marginRight: '1.5rem'}} />
+                    {t('projects.dslviz.solution-1')}
+                </Typography>
+                <Typography style={{marginTop: '5px'}}>
+                    <FontAwesomeIcon icon={faDotCircle} color={'#4497e2'} size={'lg'} style={{marginRight: '1.5rem'}} />
+                    {t('projects.dslviz.solution-2')}
+                </Typography>
+                <Typography style={{marginTop: '5px'}}>
+                    <FontAwesomeIcon icon={faDotCircle} color={'#4497e2'} size={'lg'} style={{marginRight: '1.5rem'}} />
+                    {t('projects.dslviz.solution-3')}
+                </Typography>
+                <Typography style={{marginTop: '5px'}}>
+                    <FontAwesomeIcon icon={faDotCircle} color={'#4497e2'} size={'lg'} style={{marginRight: '1.5rem'}} />
+                    {t('projects.dslviz.solution-4')}
+                </Typography>
+            </div>
         </ProjectLayout>
     );
 };
