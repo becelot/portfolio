c5bea9641e1c9036e78f1d56c4ca5abe992f28d7
Benedict Becker <benedict.becker@rwth-aachen.de>
2019-07-23 15:39:06 +0200

    Added source repositories for mensa

--- src/components/projects/MensaApp.tsx
+++ src/components/projects/MensaApp.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import {WithTranslation, withTranslation} from "react-i18next";
 import {Typography} from "@material-ui/core";
 import {faEye} from "@fortawesome/free-regular-svg-icons";
-import {faCodeBranch} from "@fortawesome/free-solid-svg-icons";
+import {faCodeBranch, faServer} from "@fortawesome/free-solid-svg-icons";
 import {ProjectPreviewButton} from "./components";
 import ProjectLayout from "./ProjectLayout";
 import ProjectDefaultContent from "./ProjectDefaultContent";
+import {faAndroid} from "@fortawesome/free-brands-svg-icons";
 
 const MensaApp: React.FC<WithTranslation> = ({t}) => {
     return (
@@ -13,7 +14,19 @@ const MensaApp: React.FC<WithTranslation> = ({t}) => {
             technologyBadges={['Android', 'Java', 'Scala', 'Play Framework', 'HTML', 'RegEx', 'Socket', 'HTTP', 'XML', 'JSON']}
             image={'projects/mensa-app.png'}
             projectPrefix={'mensa'}
-            livePreview={'http://google.de'}
+            sourcePreview={[
+                {
+                    link: 'https://github.com/becelot/MensaApp',
+                    name: 'Android',
+                    icon: faAndroid
+                },
+                {
+                    link: 'https://github.com/becelot/MensaAppBackend',
+                    name: 'Backend',
+                    icon: faServer,
+                    iconSize: '3x'
+                }
+            ]}
         >
         </ProjectLayout>
     );
