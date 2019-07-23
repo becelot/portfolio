5b0ba29aed4d160f53a7baa426ad36bcbe00c57a
Benedict Becker <benedict.becker@rwth-aachen.de>
2019-07-23 10:37:19 +0200

    DHT translations

--- src/components/projects/DeckHistoryTracker.tsx
+++ src/components/projects/DeckHistoryTracker.tsx
@@ -3,6 +3,8 @@ import {WithTranslation, withTranslation} from "react-i18next";
 import ProjectLayout from "./ProjectLayout";
 import ProjectDefaultContent from "./ProjectDefaultContent";
 import {Typography} from "@material-ui/core";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faDotCircle} from "@fortawesome/free-regular-svg-icons";
 
 const DeckHistoryTracker: React.FC<WithTranslation> = ({t}) => {
     return (
@@ -14,21 +16,37 @@ const DeckHistoryTracker: React.FC<WithTranslation> = ({t}) => {
             <Typography variant={'h6'}>
                 {t('projects.dht.project')}
             </Typography>
-            <Typography>
-                {t('projects.dht.short_description')}
-            </Typography> <br />
+            <Typography dangerouslySetInnerHTML={{__html: t(`projects.dht.short_description`)}} />
+
+            <br />
             <Typography variant={'h6'}>
                 {t('projects.dht.problem')}
             </Typography>
-            <Typography>
-                {t('projects.dht.problem_description')}
-            </Typography> <br />
+            <Typography dangerouslySetInnerHTML={{__html: t(`projects.dht.problem_description`)}}/>
+            <br />
             <Typography variant={'h6'}>
                 {t('projects.dht.solution')}
             </Typography>
             <Typography>
                 {t('projects.dht.solution_description')}
             </Typography> <br />
+            <div style={{paddingLeft: '24px'}}>
+                <Typography style={{display: 'flex', flexDirection: 'row'}}>
+                    <FontAwesomeIcon icon={faDotCircle} color={'#4497e2'} size={'lg'} style={{marginRight: '1.5rem'}} />
+                    <div>
+                        {t('projects.dht.solution-0')}</div>
+                </Typography>
+                <Typography style={{display: 'flex', flexDirection: 'row', marginTop: '5px'}}>
+                    <FontAwesomeIcon icon={faDotCircle} color={'#4497e2'} size={'lg'} style={{marginRight: '1.5rem'}} />
+                    <div dangerouslySetInnerHTML={{__html: t(`projects.dht.solution-1`)}}>
+                    </div>
+                </Typography>
+                <Typography style={{display: 'flex', flexDirection: 'row', marginTop: '5px'}}>
+                    <FontAwesomeIcon icon={faDotCircle} color={'#4497e2'} size={'lg'} style={{marginRight: '1.5rem'}} />
+                    <div>
+                        {t('projects.dht.solution-2')}</div>
+                </Typography>
+            </div>
         </ProjectLayout>
     );
 };
