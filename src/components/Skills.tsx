f1db597258240b99bf31b5501dfb800315a37729
Benedict Becker <benedict.becker@rwth-aachen.de>
2019-08-02 11:10:05 +0200

    Added skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,13 +1,14 @@
 import {WithTranslation, withTranslation} from "react-i18next";
 import React from "react";
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 import withAnimationTrigger from "../utils/withAnimationTrigger";
 import SlideInBottom from "../animations/SlideInBottom";
 import media from "../utils/media";
 import Expand from "../animations/Expand";
-import {ProjectCard} from "./projects/components";
 import {ProjectHeader} from "./projects/ProjectLayout";
-import {Divider, Typography} from "@material-ui/core";
+import {Card, Divider, Typography, withStyles} from "@material-ui/core";
+import SlideInRight from "../animations/SlideInRight";
+import StaggerInView from "../utils/StaggerInView";
 
 const Wrapper = styled.div`
   width: 100%;
@@ -102,36 +103,53 @@ interface TechBannerOptions {
 
 const TechBanner: React.FC<TechBannerOptions> = ({logo, name}) => {
     return (
-        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', flex: '0 0 16.6%', marginBottom: '25px', minWidth: '140px'}}>
+        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', flex: '0 0 13%', marginBottom: '25px', minWidth: '140px'}}>
             <img style={{height: '100px', marginBottom: '10px'}} src={logo} alt={name} />
             {name}
         </div>
     );
 };
 
+export const ProjectCard = withStyles({
+    root: {
+        margin: '10px',
+        width: '80vw',
+        maxWidth: '1280px',
+        height: '100%',
+        '@media (max-width: 768px)': {
+            maxWidth: '95vw',
+            minWidth: '95vw'
+        }
+    }
+})(Card);
+
+const cardItemStyle = css`
+    margin: 30px;
+`;
 
 const Skills: React.FC<WithTranslation> = ({t}) => {
     return (
         <Wrapper>
             <Header>{t('skills.header')}</Header>
             <Content>
-                <ProjectCard style={{padding: '20px'}}>
-                    <Typography variant={'h4'} style={{color: '#795548', margin: '2rem'}}>Application Development</Typography>
+                <StaggerInView itemStyle={cardItemStyle} animation={SlideInRight} stagger={0.2}>
+                <ProjectCard style={{padding: '20px 20px 0 20px'}}>
+                    <Typography variant={'h4'} style={{color: '#795548', margin: '2rem'}}>{t('skills.appdev')}</Typography>
                     <Divider style={{margin: '-5px 0 5px 0', marginBottom: '20px'}}/>
                     <Row>
                         <TechBanner logo={'icons/CSharp.svg'} name={'C#'}/>
                         <TechBanner logo={'icons/wpf.png'} name={'WPF'}/>
                         <TechBanner logo={'icons/java.svg'} name={'Java'}/>
+                        <TechBanner logo={'icons/android.svg'} name={'Android'}/>
                         <TechBanner logo={'icons/python.svg'} name={'Python'}/>
-
-                        <TechBanner logo={'icons/R.svg'} name={'R'}/>
-                        <TechBanner logo={'icons/Matlab.png'} name={'MATLAB'}/>
                         <TechBanner logo={'icons/delphi.jpg'} name={'Delphi'}/>
                         <TechBanner logo={'icons/haskell.png'} name={'Haskell'}/>
+                        <TechBanner logo={'icons/Matlab.png'} name={'MATLAB'}/>
+                        <TechBanner logo={'icons/R.svg'} name={'R'}/>
                     </Row>
                 </ProjectCard>
-                <ProjectCard style={{padding: '20px'}}>
-                    <Typography variant={'h4'} style={{color: '#795548', margin: '2rem'}}>Web Technologies</Typography>
+                <ProjectCard style={{padding: '20px 20px 0 20px'}}>
+                    <Typography variant={'h4'} style={{color: '#795548', margin: '2rem'}}>{t('skills.webtech')}</Typography>
                     <Divider style={{margin: '-5px 0 5px 0', marginBottom: '20px'}}/>
                     <Row>
                         <TechBanner logo={'icons/html5.svg'} name={'HTML5'}/>
@@ -141,10 +159,13 @@ const Skills: React.FC<WithTranslation> = ({t}) => {
                         <TechBanner logo={'icons/ts.png'} name={'TypeScript'}/>
                         <TechBanner logo={'icons/angular.svg'} name={'Angular'}/>
                         <TechBanner logo={'icons/react.svg'} name={'React'}/>
+                        <TechBanner logo={'icons/jquery.svg'} name={'jQuery'}/>
+                        <TechBanner logo={'icons/nodejs.svg'} name={'NodeJS'}/>
                         <TechBanner logo={'icons/enyo.png'} name={'Enyo'}/>
+
                     </Row>
                 </ProjectCard>
-                <ProjectCard style={{padding: '20px'}}>
+                <ProjectCard style={{padding: '20px 20px 0 20px'}}>
                     <Typography variant={'h4'} style={{color: '#795548', margin: '2rem'}}>Backend</Typography>
                     <Divider style={{margin: '-5px 0 5px 0', marginBottom: '20px'}}/>
                     <Row>
@@ -158,7 +179,7 @@ const Skills: React.FC<WithTranslation> = ({t}) => {
                         <TechBanner logo={'icons/celery.png'} name={'Celery'}/>
                     </Row>
                 </ProjectCard>
-                <ProjectCard style={{padding: '20px'}}>
+                <ProjectCard style={{padding: '20px 20px 0 20px'}}>
                     <Typography variant={'h4'} style={{color: '#795548', margin: '2rem'}}>Build-Tools</Typography>
                     <Divider style={{margin: '-5px 0 5px 0', marginBottom: '20px'}}/>
                     <Row>
@@ -167,8 +188,8 @@ const Skills: React.FC<WithTranslation> = ({t}) => {
                         <TechBanner logo={'icons/gradle.svg'} name={'Gradle'}/>
                     </Row>
                 </ProjectCard>
-                <ProjectCard style={{padding: '20px'}}>
-                    <Typography variant={'h4'} style={{color: '#795548', margin: '2rem'}}>Andere</Typography>
+                <ProjectCard style={{padding: '20px 20px 0 20px'}}>
+                    <Typography variant={'h4'} style={{color: '#795548', margin: '2rem'}}>{t('skills.other')}</Typography>
                     <Divider style={{margin: '-5px 0 5px 0', marginBottom: '20px'}}/>
                     <Row>
                         <TechBanner logo={'icons/git.png'} name={'Git'}/>
@@ -180,6 +201,7 @@ const Skills: React.FC<WithTranslation> = ({t}) => {
 
                     </Row>
                 </ProjectCard>
+                </StaggerInView>
             </Content>
         </Wrapper>
     );
